4ba477db2c14a71f8b114c2073bd9650c19ceba2
Stephen <stephen.sunday@andela.com>
2016-05-05 02:40:39 +0100

    Refactor how package list is generated

--- gdlsc.go
+++ gdlsc.go
@@ -1,95 +1,142 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"text/tabwriter"
 
 	"github.com/ryanuber/go-license"
 )
 
 const root = "vendor/"
 
-var files []string
-var rootPath string
-var noLicense = map[string]bool{}
-
 // Response contains license and file info
-type Response struct {
+type response struct {
 	License string
 	File    string
 }
 
-func getLicense(input chan string, output chan Response) {
-	for file := range input {
-		l, err := license.NewFromFile(file)
-		splitFile := strings.Split(file, "/")
-		splitFile = splitFile[1 : len(splitFile)-1]
-		file := path.Join(splitFile...)
-		if err != nil {
-			output <- Response{File: file, License: err.Error()}
-		} else {
-			output <- Response{File: file, License: l.Type}
-		}
-	}
-}
-func checkLicense(path string) bool {
-	path = strings.ToUpper(path)
-	return strings.Contains(path, "LICENSE") || strings.Contains(path, "COPYING")
+type request struct {
+	path        string
+	licenseFile string
 }
 
-func walker(filePath string, f os.FileInfo, err error) error {
+func catch(err error) {
 	if err != nil {
-		fmt.Println("No vendor folder found. Aborting..")
-		return err
+		panic(err)
 	}
+}
+func getLicense(input chan request, output chan response, size int) {
+	for i := range input {
+		file := strings.TrimPrefix(i.path, root)
+		if i.licenseFile == "" {
+			output <- response{File: file, License: "FILE HAS NO LICENSE"}
+		} else {
+			l, err := license.NewFromFile(i.licenseFile)
+			if err != nil {
+				f, err := os.Open(i.licenseFile)
+				catch(err)
+				dat, err := bufio.NewReader(f).Peek(size)
+				catch(err)
+				output <- response{File: file, License: "#" + string(dat)}
+			} else {
+				output <- response{File: file, License: l.Type}
+			}
+		}
+	}
+}
 
-	if !f.IsDir() {
-		// checks that a path is the root path and assigns it to rootpath.
-		// Determines root path by checking the directory of the first file seen after a change in the previous root path
-		if currentDir := path.Dir(filePath); !strings.Contains(currentDir, ".git") && (!strings.HasPrefix(currentDir, rootPath) || rootPath == "") {
-			rootPath = currentDir
-			noLicense[rootPath] = true
-		}
-		rawPath := path.Base(filePath)
-		if checkLicense(rawPath) {
-			//removes current rootpath from no license list once a license is seen in one of the rootpaths subdirectories
-			delete(noLicense, rootPath)
-			files = append(files, filePath)
+func isLicense(fileName string) bool {
+	fileName = strings.ToUpper(fileName)
+	return strings.Contains(fileName, "LICENSE") || strings.Contains(fileName, "COPYING")
+}
+
+func isPackage(filePath string) bool {
+	files, err := ioutil.ReadDir(filePath)
+	if err != nil {
+		return false
+	}
+	for _, file := range files {
+		if file.Name() == ".git" {
+			return true
 		}
 	}
-	return nil
+	return false
+}
+
+func getLicenseFile(filePath string) string {
+	files, err := ioutil.ReadDir(filePath)
+	if err != nil {
+		return ""
+	}
+	for _, file := range files {
+		if isLicense(file.Name()) {
+			return path.Join(filePath, file.Name())
+		}
+	}
+	return ""
+}
+
+func getPackageLicenses() map[string]string {
+	var licenseInfo = map[string]string{}
+
+	filepath.Walk(root, func(filePath string, f os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println("No vendor folder found. Aborting..")
+			return err
+		}
+
+		if isPackage(filePath) {
+			licenseInfo[filePath] = getLicenseFile(filePath)
+		} else if isLicense(filePath) && f.Mode().IsRegular() {
+			licenseInfo[path.Dir(filePath)] = filePath
+		}
+
+		return nil
+	})
+	return licenseInfo
 }
 
 func main() {
-	input := make(chan string)
-	output := make(chan Response)
+	input := make(chan request)
+	output := make(chan response)
 
-	filepath.Walk(root, walker)
+	packageFiles := getPackageLicenses()
 
+	size := 100
+	if len(os.Args) > 1 {
+		size, _ = strconv.Atoi(os.Args[1])
+	}
 	for a := 0; a < 5; a++ {
-		go getLicense(input, output)
+		go getLicense(input, output, size)
 	}
 
-	if len(files) == 0 { // checks that a license file was gotten
+	if len(packageFiles) == 0 { // checks that a license file was gotten
 		return
 	}
 
 	go func() {
-		for _, v := range files {
-			input <- v
+		for k, v := range packageFiles {
+			input <- request{
+				path:        k,
+				licenseFile: v,
+			}
 		}
 	}()
-
-	for i := 0; i < len(files); i++ {
+	w := tabwriter.NewWriter(os.Stdout, 1, 4, 2, ' ', 0)
+	for i := 0; i < len(packageFiles); i++ {
 		out := <-output
-		fmt.Printf("%v =======> %v\n", out.File, out.License)
+		_, err := w.Write([]byte(out.File + "\t" + out.License + "\n"))
+		if err != nil {
+			panic(err)
+		}
 	}
+	w.Flush()
 
-	for key := range noLicense {
-		key = strings.Replace(key, root, "", 1)
-		fmt.Printf("Packages without a License file: %v\n", key)
-	}
 }
